9f5ada4f300bfd79bef097fc969ee17aa6c6298f
gabrielratao <gabiratao28@gmail.com>
2023-03-29 15:23:01 -0300

    js file mobile alert test

---
+++ scripts.js
@@ -0,0 +1,5 @@
+if (/Mobi/.test(navigator.userAgent)) {
+    // Verifica se o agente do usuário contém "Mobi", que é comum em dispositivos móveis
+    alert("Esse sistema não está pronto para ser utilizado em dispositivos móveis!");
+  }
+
